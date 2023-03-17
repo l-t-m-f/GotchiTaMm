eac3b28a4f94ec360e1ec88606257337d3a4aa61
L_T_M_F <ltmf1990@protonmail.com>
2023-03-16 22:02:59 -0400

    Basic handling of keyboard input.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using static SDL2.SDL;
 
 using System.Runtime.InteropServices;
+using System.Drawing;
 
 namespace GotchiTaMm
 {
@@ -10,38 +11,38 @@ namespace GotchiTaMm
         static IntPtr Window;
         static IntPtr Renderer;
         static bool Continue = true;
+        static int[] Keyboard = new int[255];
+
+        // Structure
+        static SDL_Rect my_rectangle = new SDL_Rect { x = 50, y = 50, w = 200, h = 60 };
+
+        // Structure & Pointer to it
+        static SDL_Rect my_rectangle2 = new SDL_Rect { x = 250, y = 250, w = 20, h = 60 };
+        static IntPtr rectangle_ptr = IntPtr.Zero;
+
+        // For input
+        public delegate void KeySymbolDelegate(SDL_Keysym keysym);
+        public static event KeySymbolDelegate KeyDownEvent;
+        public static event KeySymbolDelegate KeyUpEvent;
+
 
         static void Main(string[] args)
         {
             Setup();
 
-            // Structure
-            SDL_Rect my_rectangle = new SDL_Rect { x = 50, y = 50, w = 200, h = 60 };
-
-            // Structure & Pointer to it
-            SDL_Rect my_rectangle2 = new SDL_Rect { x = 250, y = 250, w = 20, h = 60 };
             int size = Marshal.SizeOf(my_rectangle2);
-            IntPtr rectangle_ptr = Marshal.AllocHGlobal(size);
+            rectangle_ptr = Marshal.AllocHGlobal(size);
             Marshal.StructureToPtr(my_rectangle2, rectangle_ptr, false);
+
+            // Subscribe the respective methods to the events
+            // Methods have to respect the delegate definition
+            KeyDownEvent += OnKeyDown;
+            KeyUpEvent += OnKeyUp;
 
             while (Continue)
             {
-                SDL_SetRenderDrawColor(Renderer, 155, 155, 155, 255);
-                SDL_RenderClear(Renderer);
-
-                SDL_SetRenderDrawColor(Renderer, 125, 205, 235, 255);
-                SDL_RenderDrawLine(Renderer, 5, 5, 300, 310);
-
-
-                SDL_SetRenderDrawColor(Renderer, 255, 0, 0, 255);
-                // Draw with structure ref
-                SDL_RenderFillRect(Renderer, ref my_rectangle);
-
-                SDL_SetRenderDrawColor(Renderer, 0, 255, 0, 255);
-                // Draw with pointer to structure
-                SDL_RenderFillRect(Renderer, rectangle_ptr);
-
-                SDL_RenderPresent(Renderer);
+                Input();
+                Render();
             }
 
             Marshal.FreeHGlobal(rectangle_ptr);
@@ -75,6 +76,68 @@ namespace GotchiTaMm
                 Console.WriteLine(
                     $"There was an issue creating the renderer:\n{SDL_GetError()}");
             }
+        }
+
+        static void Render()
+        {
+            SDL_SetRenderDrawColor(Renderer, 155, 155, 155, 255);
+            SDL_RenderClear(Renderer);
+
+            SDL_SetRenderDrawColor(Renderer, 125, 205, 235, 255);
+            SDL_RenderDrawLine(Renderer, 5, 5, 300, 310);
+
+
+            SDL_SetRenderDrawColor(Renderer, 255, 0, 0, 255);
+            // Draw with structure ref
+            SDL_RenderFillRect(Renderer, ref my_rectangle);
+
+            SDL_SetRenderDrawColor(Renderer, 0, 255, 0, 255);
+            // Draw with pointer to structure
+            SDL_RenderFillRect(Renderer, rectangle_ptr);
+
+            SDL_RenderPresent(Renderer);
+        }
+
+        static void Input()
+        {
+            while(SDL_PollEvent(out SDL_Event e) == 1)
+            {
+                switch(e.type)
+                {
+                    case SDL_EventType.SDL_QUIT:
+                        SDL_Quit();
+                        Environment.Exit(0);
+                        break;
+                    case SDL_EventType.SDL_KEYDOWN:
+                        KeyDownEvent?.Invoke(e.key.keysym);
+                        Keyboard[(int)e.key.keysym.scancode] = 1;
+                        break;
+                    case SDL_EventType.SDL_KEYUP:
+                        KeyUpEvent?.Invoke(e.key.keysym);
+                        Keyboard[(int)e.key.keysym.scancode] = 0;
+                        break;
+                    default:
+                        // error...
+                        break;
+
+                }
+            }
+        }
+
+        public static void OnKeyDown(SDL_Keysym keysym)
+        {
+            Console.WriteLine($"Key down: {keysym.scancode}");
+
+            if(keysym.scancode == SDL_Scancode.SDL_SCANCODE_ESCAPE)
+            {
+                SDL_Quit();
+                Environment.Exit(0);
+            }
+        }
+
+        public static void OnKeyUp(SDL_Keysym keysym)
+        {
+            Console.WriteLine($"Key up: {keysym.scancode}");
         }
     }
 }
