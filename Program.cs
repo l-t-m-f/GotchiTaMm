5359d654a708fb814ccfafd9418185febe1d9137
L_T_M_F <ltmf1990@protonmail.com>
2023-03-16 21:18:42 -0400

    Basic initialization of SDL.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,38 @@ namespace GotchiTaMm
 {
     internal class Program
     {
+
+        static IntPtr Window;
+        static IntPtr Renderer;
+
         static void Main(string[] args)
         {
-            
+            if(SDL_Init(SDL_INIT_VIDEO) < 0)
+            {
+                Console.WriteLine(
+                    $"There was an issue starting SDL:\n{SDL_GetError()}!" );
+            }
+
+            Window = SDL_CreateWindow("GotchiTaMm!!!",
+                SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                640, 480, SDL_WindowFlags.SDL_WINDOW_SHOWN);
+
+            if(Window == IntPtr.Zero)
+            {
+                Console.WriteLine(
+                    $"There was an issue creating the window:\n{SDL_GetError()}");
+            }
+
+            Renderer = SDL_CreateRenderer(Window, -1, 
+                SDL_RendererFlags.SDL_RENDERER_ACCELERATED | SDL_RendererFlags.SDL_RENDERER_PRESENTVSYNC);
+
+            if(Renderer == IntPtr.Zero)
+            {
+                Console.WriteLine(
+                    $"There was an issue creating the renderer:\n{SDL_GetError()}" );
+            }
+
+            Console.WriteLine("Program exited successfully!");
         }
     }
 }
